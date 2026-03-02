371ef9562c26b2b963cd8637833f13f844a04cac
Edward Chang <skw10615@gmail.com>
2026-03-02 18:56:33 +1100

    Initial Commit

---
+++ BlockPuzzleTests/BlockPuzzleTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlockPuzzleTests.swift
+//  BlockPuzzleTests
+//
+//  Created by Edward Edward on 2/3/2026.
+//
+
+import Testing
+@testable import BlockPuzzle
+
+struct BlockPuzzleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
